af200065632b62fd42c2abab99f4370ef67cc9c1
Jakub Czakon <jakub.czakon@pascal01.intra.codilime.com>
2018-04-15 00:52:17 +0200

    updated with dsb and talking data

--- steps/sklearn/models.py
+++ steps/sklearn/models.py
@@ -1,15 +1,102 @@
+from attrdict import AttrDict
 import numpy as np
 import sklearn.linear_model as lr
 from sklearn import svm
-from sklearn.ensemble import RandomForestClassifier
+from sklearn import ensemble
 from sklearn.externals import joblib
 from catboost import CatBoostClassifier
 from xgboost import XGBClassifier
+import lightgbm as lgb
 
 from steps.base import BaseTransformer
 from steps.utils import get_logger
 
 logger = get_logger()
+
+
+class SklearnClassifier(BaseTransformer):
+    def __init__(self, estimator):
+        self.estimator = estimator
+
+    def fit(self, X, y, **kwargs):
+        self.estimator.fit(X, y)
+        return self
+
+    def transform(self, X, y=None, **kwargs):
+        prediction = self.estimator.predict_proba(X)
+        return {'prediction': prediction}
+
+
+class SklearnRegressor(BaseTransformer):
+    def __init__(self, estimator):
+        self.estimator = estimator
+
+    def fit(self, X, y, **kwargs):
+        self.estimator.fit(X, y)
+        return self
+
+    def transform(self, X, y=None, **kwargs):
+        prediction = self.estimator.predict(X)
+        return {'prediction': prediction}
+
+
+class SklearnTransformer(BaseTransformer):
+    def __init__(self, estimator):
+        self.estimator = estimator
+
+    def fit(self, X, y, **kwargs):
+        self.estimator.fit(X, y)
+        return self
+
+    def transform(self, X, y=None, **kwargs):
+        transformed = self.estimator.transform(X)
+        return {'transformed': transformed}
+
+
+class SklearnPipeline(BaseTransformer):
+    def __init__(self, estimator):
+        self.estimator = estimator
+
+    def fit(self, X, y, **kwargs):
+        self.estimator.fit(X, y)
+        return self
+
+    def transform(self, X, y=None, **kwargs):
+        transformed = self.estimator.transform(X)
+        return {'transformed': transformed}
+
+
+class LightGBM(BaseTransformer):
+    def __init__(self, model_params, training_params):
+        self.model_params = model_params
+        self.training_params = AttrDict(training_params)
+        self.evaluation_function = None
+
+    def fit(self, X, y, X_valid, y_valid, feature_names, categorical_features, **kwargs):
+        train = lgb.Dataset(X, label=y,
+                            feature_name=feature_names,
+                            categorical_feature=categorical_features
+                            )
+        valid = lgb.Dataset(X_valid, label=y_valid,
+                            feature_name=feature_names,
+                            categorical_feature=categorical_features
+                            )
+
+        evaluation_results = {}
+        self.estimator = lgb.train(self.model_params,
+                                   train,
+                                   valid_sets=[train, valid],
+                                   valid_names=['train', 'valid'],
+                                   evals_result=evaluation_results,
+                                   num_boost_round=self.training_params.number_boosting_rounds,
+                                   early_stopping_rounds=self.training_params.early_stopping_rounds,
+                                   verbose_eval=10,
+                                   feval=self.evaluation_function)
+        return self
+
+    def transform(self, X, y=None, **kwargs):
+        prediction = self.estimator.predict(X)
+        return {'prediction': prediction}
 
 
 class MultilabelEstimator(BaseTransformer):
@@ -90,7 +177,7 @@ class LinearSVCMultilabel(MultilabelEstimator):
 class RandomForestMultilabel(MultilabelEstimator):
     @property
     def estimator(self):
-        return RandomForestClassifier
+        return ensemble.RandomForestClassifier
 
 
 class CatboostClassifierMultilabel(MultilabelEstimator):
@@ -103,3 +190,18 @@ class XGBoostClassifierMultilabel(MultilabelEstimator):
     @property
     def estimator(self):
         return XGBClassifier
+
+
+def make_transformer(estimator, mode='classifier'):
+    if mode == 'classifier':
+        transformer = SklearnClassifier(estimator)
+    elif mode == 'regressor':
+        transformer = SklearnRegressor(estimator)
+    elif mode == 'transformer':
+        transformer = SklearnTransformer(estimator)
+    elif mode == 'pipeline':
+        transformer = SklearnPipeline(estimator)
+    else:
+        raise NotImplementedError("""Only classifier, regressor and transformer modes are available""")
+
+    return transformer
