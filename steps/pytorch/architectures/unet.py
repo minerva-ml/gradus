ac932e7a30214e43f3bbe7be4db687a9dc0df5d3
apyskir <andrzej.pyskir@gmail.com>
2018-04-20 11:36:23 +0200

    * added: get_upsample_pad, get_downsample_pad * added: multiclass_segmentation_loss

--- steps/pytorch/architectures/unet.py
+++ steps/pytorch/architectures/unet.py
@@ -1,13 +1,20 @@
 import torch
 import torch.nn as nn
+from .utils import get_downsample_pad, get_upsample_pad
 
 
 class UNet(nn.Module):
-    def __init__(self, conv_kernel,
-                 pool_kernel, pool_stride,
-                 repeat_blocks, n_filters,
-                 batch_norm, dropout,
-                 in_channels, **kwargs):
+    def __init__(self, conv_kernel=3,
+                 pool_kernel=3, pool_stride=2,
+                 repeat_blocks=2, n_filters=8,
+                 batch_norm=True, dropout=0.1,
+                 in_channels=3, out_channels=2,
+                 kernel_scale=3,
+                 **kwargs):
+
+        assert conv_kernel % 2 == 1
+        assert pool_stride > 1 or pool_kernel % 2 == 1
+
         super(UNet, self).__init__()
 
         self.conv_kernel = conv_kernel
@@ -18,6 +25,8 @@ class UNet(nn.Module):
         self.batch_norm = batch_norm
         self.dropout = dropout
         self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_scale = kernel_scale
 
         self.input_block = self._input_block()
         self.down_convs = self._down_convs()
@@ -44,38 +53,45 @@ class UNet(nn.Module):
 
     def _down_pools(self):
         down_pools = []
+        padding = get_downsample_pad(stride=self.pool_stride, kernel=self.pool_kernel)
         for _ in range(self.repeat_blocks):
-            down_pools.append(nn.Sequential(nn.ConstantPad2d(1, 0),
-                                            nn.MaxPool2d(kernel_size=(self.pool_kernel, self.pool_kernel),
-                                                         stride=self.pool_stride)))
+            down_pools.append(nn.MaxPool2d(kernel_size=self.pool_kernel,
+                                           stride=self.pool_stride,
+                                           padding=padding))
         return nn.ModuleList(down_pools)
 
     def _up_samples(self):
         up_samples = []
+        kernel_scale = self.kernel_scale
+        stride = self.pool_stride
+        kernel_size = kernel_scale * stride
+        padding, output_padding = get_upsample_pad(stride=stride, kernel=kernel_size)
         for i in range(self.repeat_blocks):
             in_channels = int(self.n_filters * 2 ** (i + 2))
             out_channels = int(self.n_filters * 2 ** (i + 1))
             up_samples.append(nn.ConvTranspose2d(in_channels=in_channels,
                                                  out_channels=out_channels,
-                                                 kernel_size=2,
-                                                 stride=2,
-                                                 padding=0,
-                                                 output_padding=0,
+                                                 kernel_size=kernel_size,
+                                                 stride=stride,
+                                                 padding=padding,
+                                                 output_padding=output_padding,
                                                  bias=False
                                                  ))
         return nn.ModuleList(up_samples)
 
     def _input_block(self):
+        stride = 1
+        padding = get_downsample_pad(stride=stride, kernel=self.conv_kernel)
         if self.batch_norm:
             input_block = nn.Sequential(nn.Conv2d(in_channels=self.in_channels, out_channels=self.n_filters,
                                                   kernel_size=(self.conv_kernel, self.conv_kernel),
-                                                  stride=1, padding=1),
+                                                  stride=stride, padding=padding),
                                         nn.BatchNorm2d(num_features=self.n_filters),
                                         nn.ReLU(),
 
                                         nn.Conv2d(in_channels=self.n_filters, out_channels=self.n_filters,
                                                   kernel_size=(self.conv_kernel, self.conv_kernel),
-                                                  stride=1, padding=1),
+                                                  stride=stride, padding=padding),
                                         nn.BatchNorm2d(num_features=self.n_filters),
                                         nn.ReLU(),
 
@@ -84,12 +100,12 @@ class UNet(nn.Module):
         else:
             input_block = nn.Sequential(nn.Conv2d(in_channels=self.in_channels, out_channels=self.n_filters,
                                                   kernel_size=(self.conv_kernel, self.conv_kernel),
-                                                  stride=1, padding=1),
+                                                  stride=stride, padding=padding),
                                         nn.ReLU(),
 
                                         nn.Conv2d(in_channels=self.n_filters, out_channels=self.n_filters,
                                                   kernel_size=(self.conv_kernel, self.conv_kernel),
-                                                  stride=1, padding=1),
+                                                  stride=stride, padding=padding),
                                         nn.ReLU(),
 
                                         nn.Dropout(self.dropout),
@@ -103,37 +119,39 @@ class UNet(nn.Module):
 
     def _classification_block(self):
         in_block = int(2 * self.n_filters)
+        stride = 1
+        padding = get_downsample_pad(stride=stride, kernel=self.conv_kernel)
 
         if self.batch_norm:
             classification_block = nn.Sequential(nn.Conv2d(in_channels=in_block, out_channels=self.n_filters,
                                                            kernel_size=(self.conv_kernel, self.conv_kernel),
-                                                           stride=1, padding=1),
+                                                           stride=stride, padding=padding),
                                                  nn.BatchNorm2d(num_features=self.n_filters),
                                                  nn.ReLU(),
                                                  nn.Dropout(self.dropout),
 
                                                  nn.Conv2d(in_channels=self.n_filters, out_channels=self.n_filters,
                                                            kernel_size=(self.conv_kernel, self.conv_kernel),
-                                                           stride=1, padding=1),
+                                                           stride=stride, padding=padding),
                                                  nn.BatchNorm2d(num_features=self.n_filters),
                                                  nn.ReLU(),
                                                  )
         else:
             classification_block = nn.Sequential(nn.Conv2d(in_channels=in_block, out_channels=self.n_filters,
                                                            kernel_size=(self.conv_kernel, self.conv_kernel),
-                                                           stride=1, padding=1),
+                                                           stride=stride, padding=padding),
                                                  nn.ReLU(),
                                                  nn.Dropout(self.dropout),
 
                                                  nn.Conv2d(in_channels=self.n_filters, out_channels=self.n_filters,
                                                            kernel_size=(self.conv_kernel, self.conv_kernel),
-                                                           stride=1, padding=1),
+                                                           stride=stride, padding=padding),
                                                  nn.ReLU(),
                                                  )
         return classification_block
 
     def _output_layer(self):
-        return nn.Conv2d(in_channels=self.n_filters, out_channels=1,
+        return nn.Conv2d(in_channels=self.n_filters, out_channels=self.out_channels,
                          kernel_size=(1, 1), stride=1, padding=0)
 
     def forward(self, x):
@@ -169,6 +187,7 @@ class UNetMultitask(UNet):
                  batch_norm,
                  dropout,
                  in_channels,
+                 out_channels,
                  nr_outputs):
         super(UNetMultitask, self).__init__(conv_kernel,
                                             pool_kernel,
@@ -177,7 +196,8 @@ class UNetMultitask(UNet):
                                             n_filters,
                                             batch_norm,
                                             dropout,
-                                            in_channels)
+                                            in_channels,
+                                            out_channels)
         self.nr_outputs = nr_outputs
         output_legs = []
         for i in range(self.nr_outputs):
@@ -220,16 +240,18 @@ class DownConv(nn.Module):
         self.down_conv = self._down_conv()
 
     def _down_conv(self):
+        stride = 1
+        padding = get_downsample_pad(stride=stride, kernel=self.kernel_size)
         if self.batch_norm:
             down_conv = nn.Sequential(nn.Conv2d(in_channels=self.in_channels, out_channels=self.block_channels,
                                                 kernel_size=(self.kernel_size, self.kernel_size),
-                                                stride=1, padding=1),
+                                                stride=stride, padding=padding),
                                       nn.BatchNorm2d(num_features=self.block_channels),
                                       nn.ReLU(),
 
                                       nn.Conv2d(in_channels=self.block_channels, out_channels=self.block_channels,
                                                 kernel_size=(self.kernel_size, self.kernel_size),
-                                                stride=1, padding=1),
+                                                stride=stride, padding=padding),
                                       nn.BatchNorm2d(num_features=self.block_channels),
                                       nn.ReLU(),
 
@@ -238,12 +260,12 @@ class DownConv(nn.Module):
         else:
             down_conv = nn.Sequential(nn.Conv2d(in_channels=self.in_channels, out_channels=self.block_channels,
                                                 kernel_size=(self.kernel_size, self.kernel_size),
-                                                stride=1, padding=1),
+                                                stride=stride, padding=padding),
                                       nn.ReLU(),
 
                                       nn.Conv2d(in_channels=self.block_channels, out_channels=self.block_channels,
                                                 kernel_size=(self.kernel_size, self.kernel_size),
-                                                stride=1, padding=1),
+                                                stride=stride, padding=padding),
                                       nn.ReLU(),
 
                                       nn.Dropout(self.dropout),
@@ -266,17 +288,19 @@ class UpConv(nn.Module):
         self.up_conv = self._up_conv()
 
     def _up_conv(self):
+        stride = 1
+        padding = get_downsample_pad(stride=stride, kernel=self.kernel_size)
         if self.batch_norm:
             up_conv = nn.Sequential(nn.Conv2d(in_channels=self.in_channels, out_channels=self.block_channels,
                                               kernel_size=(self.kernel_size, self.kernel_size),
-                                              stride=1, padding=1),
+                                              stride=stride, padding=padding),
 
                                     nn.BatchNorm2d(num_features=self.block_channels),
                                     nn.ReLU(),
 
                                     nn.Conv2d(in_channels=self.block_channels, out_channels=self.block_channels,
                                               kernel_size=(self.kernel_size, self.kernel_size),
-                                              stride=1, padding=1),
+                                              stride=stride, padding=padding),
                                     nn.BatchNorm2d(num_features=self.block_channels),
                                     nn.ReLU(),
 
@@ -285,12 +309,12 @@ class UpConv(nn.Module):
         else:
             up_conv = nn.Sequential(nn.Conv2d(in_channels=self.in_channels, out_channels=self.block_channels,
                                               kernel_size=(self.kernel_size, self.kernel_size),
-                                              stride=1, padding=1),
+                                              stride=stride, padding=padding),
                                     nn.ReLU(),
 
                                     nn.Conv2d(in_channels=self.block_channels, out_channels=self.block_channels,
                                               kernel_size=(self.kernel_size, self.kernel_size),
-                                              stride=1, padding=1),
+                                              stride=stride, padding=padding),
                                     nn.ReLU(),
 
                                     nn.Dropout(self.dropout)
--- steps/pytorch/architectures/utils.py
+++ steps/pytorch/architectures/utils.py
@@ -1,4 +1,6 @@
 import torch.nn as nn
+import math
+
 
 class Reshape(nn.Module):
     def __init__(self, *shape):
@@ -7,3 +9,16 @@ class Reshape(nn.Module):
 
     def forward(self, x):
         return x.view(*self.shape)
+
+
+def get_downsample_pad(stride, kernel, dilation=1):
+    return int(math.ceil((1 - stride + dilation * kernel - 1) / 2))
+
+
+def get_upsample_pad(stride, kernel, dilation=1):
+    if kernel - stride >= 0 and (kernel - stride) % 2 == 0:
+        return (int((kernel - stride) / 2), 0)
+    elif kernel - stride < 0:
+        return (0, stride - kernel)
+    else:
+        return (int(math.ceil((kernel - stride) / 2)), 1)
