061f9183b59fc513a5d65624f2b15f87eaa3bff2
Grzegorz Łoś <grzegorz.los@codilime.com>
2018-05-18 12:09:30 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='steppy',
+      version='0.0.1',
+      description='A lightweight, open-source, Python library for fast and reproducible experimentation.',
+      url='https://github.com/minerva-ml/steppy',
+      author='minerva.ml ',
+      author_email=' ml-team@neptune.ml',
+      license='MIT',
+      packages=['steppy'],
+      zip_safe=False)
