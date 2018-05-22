1a4d8fc3857acc78c9b4430df65e3415d3dc53d1
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2018-05-23 00:16:12 +0200

    corrected setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='steppy',
-      version='0.0.1',
-      description='A lightweight, open-source, Python library for fast and reproducible experimentation.',
+      version='0.1.1',
+      description='A lightweight, open-source, Python library for fast and reproducible experimentation',
       url='https://github.com/minerva-ml/steppy',
-      author='minerva.ml ',
+      author='neptune.ml ',
       author_email=' ml-team@neptune.ml',
       license='MIT',
       packages=['steppy'],
