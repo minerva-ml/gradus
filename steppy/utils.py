9587558514b4c442743039bb5b6d169d9cb7e3c7
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2018-06-03 00:44:19 +0200

    corrected docstrings, refactored naming

--- steppy/utils.py
+++ steppy/utils.py
@@ -1,104 +1,91 @@
 import logging
-import os
 import sys
 
 import pydot_ng as pydot
 from IPython.display import Image, display
 
 
-def view_graph(graph_info):
-    """Displays graph in the notebook
-
-    Args:
-        graph_info (dict): graph dictionary with 'nodes' and 'edges' defined
-    """
-    graph = create_graph(graph_info)
-    view_pydot(graph)
-
-
-def save_graph(graph_info, filepath):
-    """Saves pydot graph to file
-
-    Args:
-        graph_info (dict): graph dictionary with 'nodes' and 'edges' defined
-        filepath (str): filepath to which the graph should be saved
-
-    """
-    graph = create_graph(graph_info)
-    graph.write(filepath, format='png')
-
-
-def view_pydot(pydot_object):
-    """Displays pydot graph in jupyter notebook
-
-    Args:
-        pydot_object: pydot.Dot object
-    """
-    plt = Image(pydot_object.create_png())
-    display(plt)
-
-
-def create_graph(graph_info):
-    """Creates pydot graph from the step graph dictionary.
-
-    Args:
-        graph_info (dict): graph dictionary with 'nodes' and 'edges' defined
-
-    Returns:
-        obj: pydot.Dot object representing the step graph
-    """
-    dot = pydot.Dot()
-    for node in graph_info['nodes']:
-        dot.add_node(pydot.Node(node))
-    for node1, node2 in graph_info['edges']:
-        dot.add_edge(pydot.Edge(node1, node2))
-    return dot
-
-
-def create_filepath(filepath):
-    """Creates directory path for the filepath if non-existend
-
-    Makes it easy to created necessary directory for the filepath.
-
-    Args:
-        filepath (str): filepath for which directory needs to be created
-
-    """
-    dirpath = os.path.dirname(filepath)
-    os.makedirs(dirpath, exist_ok=True)
-
-
 def initialize_logger():
-    """Initialize steps logger
+    """Initialize steppy logger, with name 'steppy'
 
-    It creates logger of name 'steps'
+    This logger is used throughout the steppy library to report computation progress.
+
+    Example:
+        initialize_logger()
+        logger = get_logger()
+        logger.info('My message inside pipeline')
+        result looks like this:
+        `2018-06-02 12:33:48 steppy >>> My message inside pipeline`
 
     Returns:
         logging.Logger: logger object
     """
-    logger = logging.getLogger('steps')
+    logger = logging.getLogger('steppy')
     logger.setLevel(logging.INFO)
     message_format = logging.Formatter(fmt='%(asctime)s %(name)s >>> %(message)s',
                                        datefmt='%Y-%m-%d %H:%M:%S')
 
-    # console handler for validation info
-    ch_va = logging.StreamHandler(sys.stdout)
-    ch_va.setLevel(logging.INFO)
-
-    ch_va.setFormatter(fmt=message_format)
+    # console handler
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(fmt=message_format)
 
     # add the handlers to the logger
-    logger.addHandler(ch_va)
+    logger.addHandler(console_handler)
 
     return logger
 
 
 def get_logger():
-    """Fetch existing steps logger
+    """Fetch existing steppy logger with name 'steppy'
 
-    It fetches logger of name 'steps'
+    Example:
+        initialize_logger()
+        logger = get_logger()
+        logger.info('My message inside pipeline')
+        result looks like this:
+        `2018-06-02 12:33:48 steppy >>> My message inside pipeline`
 
     Returns:
         logging.Logger: logger object
     """
-    return logging.getLogger('steps')
+    return logging.getLogger('steppy')
+
+
+def display_pipeline(structure_dict):
+    """Displays pipeline structure in the jupyter notebook
+
+    Args:
+        structure_dict (dict): dict returned by step.upstream_pipeline_structure
+    """
+    graph = _create_graph(structure_dict)
+    plt = Image(graph.create_png())
+    display(plt)
+
+
+def save_as_png(structure_dict, filepath):
+    """Saves pipeline as png
+
+    Args:
+        structure_dict (dict): dict returned by step.upstream_pipeline_structure
+        filepath (str): filepath to which the png with pipeline visualization should be saved
+    """
+    graph = _create_graph(structure_dict)
+    graph.write(filepath, format='png')
+
+
+def _create_graph(structure_dict):
+    """Creates pydot graph from the pipeline structure dict.
+
+    Args:
+        structure_dict (dict): dict returned by step.upstream_pipeline_structure
+
+    Returns:
+        obj: pydot.Dot object representing upstream pipeline structure (with regard to the current Step).
+    """
+    dot = pydot.Dot()
+    for node in structure_dict['nodes']:
+        dot.add_node(pydot.Node(node))
+    for node1, node2 in structure_dict['edges']:
+        dot.add_edge(pydot.Edge(node1, node2))
+    return dot
